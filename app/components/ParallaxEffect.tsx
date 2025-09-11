32431c1d206510b2beae0a2306ddc9c1f2eae977
FraserShimmins <shimmins.f@gmail.com>
2025-09-11 17:49:13 +0100

    Initialise Repository

---
+++ app/components/ParallaxEffect.tsx
@@ -0,0 +1,16 @@
+
+
+export default function ParallaxEffect(){
+
+    return (
+        <section className="pt-10 text-white">
+            <h1 className="text-4xl">
+                <span className="font-extrabold">HELLO</span> This is a test run of the font
+            </h1>
+
+            <p className="text-2xl">
+                HELLO, This is a test run of the font
+            </p>
+        </section>
+    );
+};
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Handjet, Tiny5 } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const handjet = Handjet({
+  variable: "--font-handjet",
+  subsets: ["latin"],
+});
+
+const tiny5 = Tiny5({
+  weight: "400",       // only available weight
+  subsets: ["latin"],
+  variable: "--font-tiny5",
+});
+
+export const metadata: Metadata = {
+  title: "Personal Blog",
+  description: "Personal Blog of Fraser Shimmins",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${handjet.variable} ${tiny5.variable} antialiased font-handjet font-semibold`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
